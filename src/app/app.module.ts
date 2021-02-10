2233b7ea00300e93d4efee90bde07492d0bf4e6e
Diana <ing.dianacmoreno@gmail.com>
2021-02-10 16:17:37 -0500

    menu funcional

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {FormsModule} from "@angular/forms";
+import {FormsModule} from '@angular/forms';
 
 import {MatMenuModule} from '@angular/material/menu';
 import { TablaComponent } from './tabla/tabla.component';
@@ -13,6 +14,9 @@ import { ButtonComponent } from './button/button.component';
 import { ListaColoresComponent } from './lista-colores/lista-colores.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SliderComponent } from './slider/slider.component';
+import { ProductoDetalleComponent } from './producto-detalle/producto-detalle.component';
+import { FormularioAgregarUsuarioComponent } from './formulario-agregar-usuario/formulario-agregar-usuario.component';
+import { ListaUsuariosComponent } from './lista-usuarios/lista-usuarios.component';
 
 
 
@@ -24,10 +28,17 @@ import { SliderComponent } from './slider/slider.component';
     ButtonComponent,
     ListaColoresComponent,
     NavbarComponent,
-    SliderComponent
+    SliderComponent,
+    ProductoDetalleComponent,
+    FormularioAgregarUsuarioComponent,
+    ListaUsuariosComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot([
+      {path: 'button', component: ButtonComponent},
+      {path: '', component: SliderComponent},
+    ]),
     BrowserAnimationsModule,
     MatMenuModule,
     FormsModule
